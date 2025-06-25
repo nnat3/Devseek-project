325f110da7539a166305402918ef086e0e6d4a42
Neathan Kungu <natekungu857@gmail.com>
2025-06-25 19:43:26 +0300

    job data fetched & added to html using DOM manipulation

---
+++ index.js
@@ -0,0 +1,21 @@
+/* Fetch job data from json file */
+document.addEventListener ("DOMContentLoaded", () => {
+      fetch("jobs.json")
+       .then((res) => res.json())
+        .then((jobs => {
+        const jobList = document.querySelector(".jobs-list")
+        
+        jobs.forEach((job)=> {
+
+         const jobCard = document.createElement("div")/* div to display job cards */
+        jobCard.classList.add("job-card");
+        jobCard.innerHTML = `<h3>${job.title} <span class="type">${job.type}</span> <p>${job.description}</p>
+         <div class="job-info">
+         <span>${job.location}</span>
+         <span>${job.salary}</span>
+         </div>`;
+
+         jobList.appendChild(jobCard)
+        });
+}))
+});
