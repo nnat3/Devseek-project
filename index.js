94f391e3b548ef76757679644ec768880f9507f2
Neathan Kungu <natekungu857@gmail.com>
2025-06-26 15:19:52 +0300

    ReferenceError in renderJobs function resolved,now the `filter jobs by location & title` is fully functionable

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 /* Declare jobs globally for easy manipulation */
 let allJobs = []
 
+/* Fetch job data from json file */
 function loadJobs() {
       fetch("jobs.json")
       .then((res) => res.json())
       .then((jobs) =>{
+            /*console.log("Loaded jobs:", jobs); */
             allJobs = jobs;
             renderJobs(jobs)
       })
 }
 
 /* Job cards display function */
-function renderJobs() {
-      const jobList = document.querySelector(".job-list");
+function renderJobs(jobs) {
+      const jobList = document.querySelector(".jobs-list");
       jobList.innerHTML = ""
-}
 
-/* Fetch job data from json file */
-document.addEventListener ("DOMContentLoaded", () => {
-      fetch("jobs.json")
-       .then((res) => res.json())
-        .then((jobs => {
-        const jobList = document.querySelector(".jobs-list")
-        
-        jobs.forEach((job)=> {
+ jobs.forEach((job)=> {
 
-         const jobCard = document.createElement("div")/* div to display job cards */
-        jobCard.classList.add("job-card");
-        jobCard.innerHTML = `<h3>${job.title} <span class="type">${job.type}</span> <p>${job.description}</p>
+  const jobCard = document.createElement("div")/* div to display job cards */
+  jobCard.classList.add("job-card");
+  jobCard.innerHTML = `<h3>${job.title} <span class="type">${job.type}</span> <p>${job.description}</p>
          <div class="job-info">
          <span>${job.location}</span>
          <span>${job.salary}</span>
@@ -35,8 +29,7 @@ document.addEventListener ("DOMContentLoaded", () => {
 
          jobList.appendChild(jobCard)
         });
-}))
-});
+      }
 
 /* Filter jobs by location & job title */
 function setupFilter() {
@@ -48,16 +41,20 @@ function setupFilter() {
 }
 
 function applyFilters() {
-      const title = document.getElementById("filter-title").toLowerCase().value
+      const title = document.getElementById("filter-title").value.toLowerCase()
       const location = document.getElementById("filter-location").value
-      const filteredResult = allJobs.filter((jobs) => {
-        const titleMatches = job.title.includes(title)
+      const filteredResult = allJobs.filter((job) => {
+        const titleMatches = job.title.toLowerCase().includes(title)
         const locationMatches = location === "all" || job.location === location;
         return titleMatches && locationMatches
       })
       renderJobs(filteredResult)
 }
  
+document.addEventListener("DOMContentLoaded", () => {
+      loadJobs();
+      setupFilter();
+})
 
 
 
